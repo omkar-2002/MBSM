a6d30e5761fa49b0911716048e942a5df5094845
omkar-2002 <omkarsalapurkar2002@gmail.com>
2023-02-23 20:27:03 +0530

    activity change

--- src/screens/activity/Activity.js
+++ src/screens/activity/Activity.js
@@ -2,14 +2,7 @@ import React from "react";
 import { useQuery } from "react-query";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase.config";
-import { Document, Page } from "react-pdf";
-function PdfComponent({ pdfLink }) {
-  return (
-    <Document file={pdfLink}>
-      <Page pageNumber={1} />
-    </Document>
-  );
-}
+import { Button } from "@mui/material";
 export default function Activity() {
   const { isLoading, data } = useQuery("activities", async () => {
     const querySnapshot = await getDocs(collection(db, "Activity"));
@@ -21,25 +14,30 @@ export default function Activity() {
 
   return (
     <div>
-      <h2 style={{textAlign:"center"}}>Activities</h2>
-      <div
-      className="pdfContainer"
-      >
+      <h2 style={{ textAlign: "center", marginTop: 30 }}>Activities</h2>
+      <div style={{ marginBottom: 30, marginTop: 30 }} className="pdfContainer">
         {data.map((item) => {
           return (
-            <div className="pdfs">
-              <a href={item?.pdfUrl} className = "pdf" target="_blank" rel="noopener noreferrer">
-                <iframe
-                  src={item?.pdfUrl}
-                  scrolling="no"
-                  width="200px"
-                  height="230px"
-                  style={{overflow: "hidden"}}
-                  
-                ></iframe>
-              </a>
-            </div>
-            
+            <a
+              href={item?.pdfUrl}
+              className="pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <iframe
+                src={item?.pdfUrl}
+                scrolling={false}
+                width="200px"
+                height="230px"
+                style={{ marginBottom: 10, overflow: "hidden" }}
+              ></iframe>
+              <Button variant="outlined">Open</Button>
+            </a>
           );
         })}
       </div>
